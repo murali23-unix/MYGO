1620d09eb2e8b6b6bbdbdf9972cec94ae6779209
Muralidharan V Krishnakumar <murali23.unix@gmail.com>
2022-09-25 17:00:30 +0530

    First Commit

---
+++ Hello-World.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+/* This is a multiline comment */
+// This is a single line comment
+
+func main() {
+
+	fmt.Printf("Hello World\n")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module C/MYGO
+
+go 1.19
